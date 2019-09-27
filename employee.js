2c822e2805428740567c2e2df24d5a43701e9e8c
Michael Williams <mwilliams2964@gmail.com>
2019-09-26 20:25:10 -0700

    added global variables

---
+++ employee.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+    
+    // Your web app's Firebase configuration
+    var firebaseConfig = {
+    apiKey: "AIzaSyDcQlUSutm5s9I30LbakulM242vDn_a_Zo",
+    authDomain: "click-counter-a5c53.firebaseapp.com",
+    databaseURL: "https://click-counter-a5c53.firebaseio.com",
+    projectId: "click-counter-a5c53",
+    storageBucket: "",
+    messagingSenderId: "190428088899",
+    appId: "1:190428088899:web:70ec1a093f22207f2c22f6"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+let database = firebase.database();
+
+let name = "";
+let role = "";
+let startDate = "";
+let monthlyRate = 0;
+// monthsWorked = currentDate - startDate
+
+name = $("#")
+
+
+database.ref().set({
+    name: 
+})
+
+
+})
